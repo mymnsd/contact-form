a3bb14bad518601ae0010f7cdeb8ec99e9903fd9
mayumi <mym.mrn@gmail.com>
2024-04-09 23:05:06 +0900

    ２回目の変更

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,13 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
     public function index(){
         return view('index');
     }
-    public function confirm(){
-        
+    // public function confirm(){
+        public function confirm(Request $request){
+        $contact = $request->only(['name', 'email', 'tel', 'content']);
+        // return $contact;
+        // return view('confirm', ['contact' => $contact]);
+        return view('confirm', compact('contact'));
     }
+    public function store(Request $request){
+        $contact = $request->only(['name', 'email', 'tel', 'content']);
+        Contact::create($contact);
+        return view('thanks');
+    }
+
 }
